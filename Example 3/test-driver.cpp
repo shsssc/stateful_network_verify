423d33a02f6f8395d4f7e10fc9bc0f951ff3c9f2
Sichen Song <you@example.com>
2022-02-19 22:15:33 -0800

    Start from node 1 instead of node 2

--- Example 3/test-driver.cpp
+++ Example 3/test-driver.cpp
@@ -5,6 +5,6 @@ int main() {
 	klee_make_symbolic(&header, sizeof(header), "header");
 
 	Topology t;
-	int outPort = t.forward(PktState(header,1,3));
+	int outPort = t.forward(PktState(header,0,3));
 	return outPort<0;
 }
