a51c33027bc6ea81dd7fd15376b0466ba11d779f
Sichen Song <you@example.com>
2022-03-07 00:22:00 -0800

    allow optimization for network generator

--- generateAP.py
+++ generateAP.py
@@ -76,72 +76,6 @@ class APDriverGenerator:
         template = jinja_env.get_template("templates/ecgen-driver.cpp")
         return template.render(nodes=self.nodes, hop=self.hop, ecs=self.ecs)
 
-
-class ECToCode:
-    def __init__(self, file: str):
-        self.file = file
-        self.sorted_disjoint_intervals: Dict[str, list] = dict()
-
-        records: Dict[str, list] = dict()
-        with open(file, 'r') as f:
-            tsv = csv.reader(f)
-            for row in tsv:
-                node = row[0]
-                prefix = row[1]
-                out_node = int(row[2])
-                out_port = int(row[3])
-                if node not in records:
-                    records[node] = []
-                records[node].append((prefix, (out_node, out_port)))
-        for node, l in records.items():
-            # intervals are already disjoint thanks to trie
-            l.sort(key=lambda x: int(x[0] + '0' * (32 - len(x[0])), 2))  # sort with interval start
-            merged_intervals = self.__merge_intervals(l)
-
-            self.sorted_disjoint_intervals[node] = merged_intervals
-
-    def __merge_intervals(self, intervals: list):
-        result = []
-        if len(intervals) == 0:
-            return
-        l = 0
-        r = l
-        while l < len(intervals):
-            while r < len(intervals) and intervals[r][1] == intervals[l][1]:
-                r += 1
-            start = int(intervals[l][0] + '0' * (32 - len(intervals[l][0])), 2)
-            end = int(intervals[r - 1][0] + '1' * (32 - len(intervals[r - 1][0])), 2)
-            result.append((start, end, intervals[l][1]))
-            l = r
-        return result
-
-    def generate_code(self, node: str):
-        return self.__generate_code(self.sorted_disjoint_intervals[node], 0,
-                                    len(self.sorted_disjoint_intervals[node]) - 1, 0)
-
-    def __generate_code(self, intervals: list, l: int, r: int, level: int):
-        if r < l:
-            return ''
-        if r == l:
-            interval = intervals[r]
-            node = interval[2][0]
-            port = interval[2][1]
-            return f"{' ' * 4 * level}return {{header, {node}, {port}}};"
-        m = (r + l) // 2
-        interval = intervals[m]
-        node = interval[2][0]
-        port = interval[2][1]
-        start = interval[0]
-        end = interval[1]
-        return f"{' ' * 4 * level}if (header.dst_address >= {hex(start)} && header.dst_address <= {hex(end)})" \
-               f" return {{header, {node}, {port}}};\n" \
-               f"{' ' * 4 * level}else if (header.dst_address < {hex(start)}) {{\n" \
-               f"{self.__generate_code(intervals, l, m - 1, level + 1)}\n" \
-               f"{' ' * 4 * level}}} else {{\n" \
-               f"{self.__generate_code(intervals, m + 1, r, level + 1)}\n" \
-               f"{' ' * 4 * level}}}"
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generates C++ model code for a network')
     parser.add_argument('-d', dest='directory', required=True, help='select config directory of the network')
@@ -157,5 +91,3 @@ if __name__ == "__main__":
     g.generate_code()
     g.execute_model()
 
-    i = ECToCode('Example 3/ecs.csv')
-    print(i.generate_code('Router1'))
--- generateNetworkCode.py
+++ generateNetworkCode.py
@@ -6,10 +6,15 @@ from generateSrcReachabilityTestDriver import SrcReachabilityDriverGenerator
 from util.snakeCase import snake_case
 import argparse
 import os
+from generateOptimizedRouterHeader import OptimizedRouterHeaderGenerator
+from typing import Set, Dict
+
 
 class NetworkGenerator:
-    def __init__(self, directory: str):
+    def __init__(self, directory: str, use_ec_opt=True):
         self.directory = directory
+        self.useEcs = use_ec_opt
+        self.routerToEcCode: Dict[str, OptimizedRouterHeaderGenerator] = dict()
         self.topologyCode = TopologyGenerator()
         self.topologyCode.add_forwarding_table(os.path.join(directory, "topology.txt"))
         self.routerCodes = {}
@@ -37,6 +42,10 @@ class NetworkGenerator:
                     class_name = item.name[:-4]
                     self.addRouterInstance(class_name)
                     self.routerCodes[class_name].add_forwarding_table(os.path.join(directory, item.name))
+                elif self.useEcs and item.name.endswith("ecs.csv"):
+                    ecs = OptimizedRouterHeaderGenerator(os.path.join(directory, item.name))
+                    for r in ecs.sorted_disjoint_intervals:
+                        self.routerToEcCode[r] = ecs
 
     def generate_code(self):
         with open(os.path.join(self.directory, "topology.h"), 'w') as f:
@@ -44,12 +53,17 @@ class NetworkGenerator:
         for e in self.routerCodes.values():
             with open(os.path.join(self.directory, snake_case(e.name) + ".h"), 'w') as routerFile:
                 routerFile.write(e.generate_code())
+        if self.useEcs:
+            for r, v in self.routerToEcCode.items():
+                with open(os.path.join(self.directory, snake_case(r) + ".h"), 'w') as routerFile:
+                    routerFile.write(v.generate_code(r))
 
 
 class ReachabilityCodeGenerator(NetworkGenerator):
-    def __init__(self, directory: str, src: str, port: int):
-        super().__init__(directory)
-        self.driverCode = SrcReachabilityDriverGenerator(src, port, hop=self.topologyCode.diameter() * 3) # TTL = diameter * 3 as best effort loop detection
+    def __init__(self, directory: str, src: str, port: int, use_ec_opt=True):
+        super().__init__(directory, use_ec_opt)
+        self.driverCode = SrcReachabilityDriverGenerator(src, port,
+                                                         hop=self.topologyCode.diameter() * 3)  # TTL = diameter * 3 as best effort loop detection
         self.driverCode.add_node_name_to_id_map(self.topologyCode.nodes)
 
     def generate_code(self):
@@ -65,8 +79,9 @@ if __name__ == "__main__":
     parser.add_argument('-d', dest='directory', required=True, help='select config directory of the network')
     parser.add_argument('-s', dest='src', required=True, help='src node')
     parser.add_argument('-p', dest='port', type=int, default=0, help='src port')
-
+    parser.add_argument('-o', dest='optimize', type=bool, default=False,
+                        help='search for csv files that treat network boxes as components to save computation')
     args = parser.parse_args()
 
-    g = ReachabilityCodeGenerator(args.directory, args.src, args.port)
+    g = ReachabilityCodeGenerator(args.directory, args.src, args.port, args.optimize)
     g.generate_code()
