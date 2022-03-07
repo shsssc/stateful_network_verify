ec6c290eae218c2223a153a3c296343146f19e34
Sichen Song <you@example.com>
2022-03-06 23:44:14 -0800

    optimizer

--- generateAP.py
+++ generateAP.py
@@ -1,4 +1,4 @@
-from typing import Set
+from typing import Set, Dict
 import os
 from util.trie import Trie
 from util.IPPrefixDecode import decode_ip_prefix
@@ -11,7 +11,7 @@ import argparse
 
 
 class APGenerator(NetworkGenerator):
-    def __init__(self, directory: str, nodes: Set[str]):
+    def __init__(self, directory: str, nodes: Set[str], out_file: str = 'ecs.csv'):
         super().__init__(directory)
         self.nodes = nodes
         self.trie = Trie()
@@ -20,6 +20,7 @@ class APGenerator(NetworkGenerator):
         self.build_trie()
         self.ecs = self.all_concrete_ec()
         self.driverCode = APDriverGenerator(nodes=nodes, hop=self.topologyCode.diameter() + 2, ecs=self.ecs)
+        self.out_file = out_file
 
     def all_ec(self):
         return self.trie.all_ec()
@@ -59,6 +60,11 @@ class APGenerator(NetworkGenerator):
         os.system('cp templates/Makefile "%s"' % self.directory)
         os.system('cp templates/common.h "%s"' % self.directory)
 
+    def execute_model(self):
+        # XXX: not using makefile
+        os.system(f'cd "%s" && c++ test-driver.cpp && ./a.out > "{self.out_file}"' % self.directory)
+        os.remove(os.path.join(self.directory, 'a.out'))
+
 
 class APDriverGenerator:
     def __init__(self, nodes: Set[str], hop: int, ecs: list):
@@ -69,6 +75,71 @@ class APDriverGenerator:
     def generate_code(self) -> str:
         template = jinja_env.get_template("templates/ecgen-driver.cpp")
         return template.render(nodes=self.nodes, hop=self.hop, ecs=self.ecs)
+
+
+class ECToCode:
+    def __init__(self, file: str):
+        self.file = file
+        self.sorted_disjoint_intervals: Dict[str, list] = dict()
+
+        records: Dict[str, list] = dict()
+        with open(file, 'r') as f:
+            tsv = csv.reader(f)
+            for row in tsv:
+                node = row[0]
+                prefix = row[1]
+                out_node = int(row[2])
+                out_port = int(row[3])
+                if node not in records:
+                    records[node] = []
+                records[node].append((prefix, (out_node, out_port)))
+        for node, l in records.items():
+            # intervals are already disjoint thanks to trie
+            l.sort(key=lambda x: int(x[0] + '0' * (32 - len(x[0])), 2))  # sort with interval start
+            merged_intervals = self.__merge_intervals(l)
+
+            self.sorted_disjoint_intervals[node] = merged_intervals
+
+    def __merge_intervals(self, intervals: list):
+        result = []
+        if len(intervals) == 0:
+            return
+        l = 0
+        r = l
+        while l < len(intervals):
+            while r < len(intervals) and intervals[r][1] == intervals[l][1]:
+                r += 1
+            start = int(intervals[l][0] + '0' * (32 - len(intervals[l][0])), 2)
+            end = int(intervals[r - 1][0] + '1' * (32 - len(intervals[r - 1][0])), 2)
+            result.append((start, end, intervals[l][1]))
+            l = r
+        return result
+
+    def generate_code(self, node: str):
+        return self.__generate_code(self.sorted_disjoint_intervals[node], 0,
+                                    len(self.sorted_disjoint_intervals[node]) - 1, 0)
+
+    def __generate_code(self, intervals: list, l: int, r: int, level: int):
+        if r < l:
+            return ''
+        if r == l:
+            interval = intervals[r]
+            node = interval[2][0]
+            port = interval[2][1]
+            return f"{' ' * 4 * level}return {{header, {node}, {port}}};"
+        m = (r + l) // 2
+        interval = intervals[m]
+        node = interval[2][0]
+        port = interval[2][1]
+        start = interval[0]
+        end = interval[1]
+        return f"{' ' * 4 * level}if (header.dst_address >= {hex(start)} && header.dst_address <= {hex(end)})" \
+               f" return {{header, {node}, {port}}};\n" \
+               f"{' ' * 4 * level}else if (header.dst_address < {hex(start)}) {{\n" \
+               f"{self.__generate_code(intervals, l, m - 1, level + 1)}\n" \
+               f"{' ' * 4 * level}}} else {{\n" \
+               f"{self.__generate_code(intervals, m + 1, r, level + 1)}\n" \
+               f"{' ' * 4 * level}}}"
 
 
 if __name__ == "__main__":
@@ -84,11 +155,7 @@ if __name__ == "__main__":
 
     g = APGenerator(args.directory, component)
     g.generate_code()
+    g.execute_model()
 
-    #component.add('Router1')
-    #component.add('Router2')
-    #component.add('Router3')
-    #a = APGenerator('Example 3', component)
-    #a.generate_code()
-    # a1 = APDriverGenerator(component, 16, a.ecs)
-    # print(a1.generate_code())
+    i = ECToCode('Example 3/ecs.csv')
+    print(i.generate_code('Router1'))
--- templates/ecgen-driver.cpp
+++ templates/ecgen-driver.cpp
@@ -3,23 +3,25 @@
 
 class Network : public Topology {
   public:
-	void forward(PktState& pktState) {
-
-		for (int hop = 0; hop < {{hop}}; hop++) {
+    void forward(PktState& pktState) {
+        for (int hop = 0; hop < {{hop}}; hop++) {
             pktState = node_execute(pktState);
-			if (pktState.port == PORT_DROP) return;
-			pktState = link_function(pktState);
+            if (pktState.port == PORT_DROP) return;
+            PktState oldState = pktState;
+            pktState = link_function(pktState);
             switch (pktState.node){
                 {% for node in nodes %}
                 case {{node}}Id:
                 {% endfor %}
                 break;
-                default: return;
+                default:
+                    pktState = oldState;
+                    return;
             }
-			if (pktState.port == PORT_DROP) return;
-		}
-		return;
-	}
+            if (pktState.port == PORT_DROP) return;
+        }
+    return;
+    }
 };
 
 int main() {
@@ -31,8 +33,8 @@ int main() {
     header.dst_address = {{ec['concrete']}};
     pkt = PktState(header, {{node}}Id, 0);
     n.forward(pkt);
-    //printf("%s,%s,%d,%d\n","{{node}}", "{{ec['prefix']}}", pkt.node, pkt.port);
-    printf("inNode: %s,prefix: %s,outNode: %d, outPort: %d\n","{{node}}", "{{ec['prefix']}}", pkt.node, pkt.port);
+    printf("%s,%s,%d,%d\n","{{node}}", "{{ec['prefix']}}", pkt.node, pkt.port);
+    //printf("inNode: %s,prefix: %s,outNode: %d, outPort: %d\n","{{node}}", "{{ec['prefix']}}", pkt.node, pkt.port);
         {% endfor %}
     {% endfor %}
 
